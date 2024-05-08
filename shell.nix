a7b7948b0221a7f227265dccb43da85be925fc6a
Kilian Braun <hello@kilianbraun.de>
2024-05-08 11:59:41 +0200

    added basic swagger yaml for reference

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    # MITM Proxy to reverse engineer apis
+    pkgs.mitmproxy
+    pkgs.mitmproxy2swagger
+
+    # Python 3.12 + packages
+    (pkgs.python312.withPackages (python-pkgs: [
+      python-pkgs.requestsn
+    ]))
+  ];
+}
