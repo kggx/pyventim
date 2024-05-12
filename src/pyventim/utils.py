81c1d83caca98990bf2b769c68752e14282fa106
Kilian Braun <hello@kilianbraun.de>
2024-05-12 13:30:26 +0200

    formatting

--- src/pyventim/utils.py
+++ src/pyventim/utils.py
@@ -2,7 +2,8 @@ import requests
 import pathlib
 from typing import Dict, Any
 
-def parse_city_name_from_link(city_url:str) -> str:
+
+def parse_city_name_from_link(city_url: str) -> str:
     """Given a link like "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880": This function will return the city name.
 
     Args:
@@ -10,10 +11,11 @@ def parse_city_name_from_link(city_url:str) -> str:
 
     Returns:
         str: The city name from the url.
-    """    
-    return pathlib.Path(city_url).parts[3].split('-')[0]
+    """
+    return pathlib.Path(city_url).parts[3].split("-")[0]
 
-def parse_city_id_from_link(city_url:str) -> int:
+
+def parse_city_id_from_link(city_url: str) -> int:
     """Given a link like "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880": This function returns the city id.
 
     Args:
@@ -22,4 +24,4 @@ def parse_city_id_from_link(city_url:str) -> int:
     Returns:
         int: The city id from the url.
     """
-    return  pathlib.Path(city_url).parts[3].split('-')[1]
+    return pathlib.Path(city_url).parts[3].split("-")[1]
