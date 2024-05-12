a2f732147e54db3ad74fb20dc098c46abf491745
Kilian Braun <hello@kilianbraun.de>
2024-05-12 10:52:55 +0200

    rewritten a lot of public code to better reflect the api

--- src/pyventim/public.py
+++ src/pyventim/public.py
@@ -1,6 +1,6 @@
 import requests
-from typing import Dict
-import pprint
+from typing import Dict, List, Literal, Any
+from datetime import datetime
 
 
 class EventimExploration:
@@ -15,29 +15,97 @@ class EventimExploration:
             "https://public-api.eventim.com/websearch/search/api/exploration"
         )
         
-    def _validate_query(self, query_parameters:Dict[str, Any]) -> bool:
+    def _build_query_parameters(self, **kwargs) -> Dict[str, Any]:
         # TODO: Docs
-        parameters = query_parameters.keys()
+        query: Dict[str, Any] = {}
+        parameters = kwargs.keys()
+        
+        # Check if atlease one of the required keywords is present
         if "search_term" not in parameters and "categories" not in parameters and "city_ids" not in parameters:
-            raise ValueError('Must have search_term, categories or city_ids in the query parameters')
-        
-        if query_parameters.get('search_term'):
-            if len(search_term) < 2:
-                raise ValueError(f"search_term must have atleast two characters...")
+            raise ValueError('Must have search_term, categories or city_ids in the query parameters!')
                 
+        # Validate if page is valid        
+        if kwargs.get('page'):
+            page: int = kwargs.get('page')
+            if page < 1:
+                raise ValueError('page must be a positive integer > 0!')
+            
+            query['page'] = page
+                
+        # Validate if sort is valid        
+        if kwargs.get('sort'):
+            sort: str = kwargs.get('sort')
+            allowed: List[str] = ['DateAsc', 'DateDesc' , 'NameAsc', 'NameDesc' , 'Rating' , 'Recommendation']
+            if sort not in allowed:
+                raise ValueError('sort must be one of the following values: "DateAsc", "DateDesc", "NameAsc", "NameDesc", "Rating", "Recommendation"!')
+            
+            query['sort'] = sort
         
-        return True        
+        # Validate if search_term is valid
+        if kwargs.get('search_term'):
+            search_term: str = kwargs.get('search_term')
+            if len(search_term) < 2:
+                raise ValueError("search_term must have atleast two characters!")
+            
+            query['search_term'] = search_term
+        
+        # Validate if categories is valid
+        if kwargs.get('categories'):
+            categories: List[str] = kwargs.get('categories')
+            if not isinstance(categories, List):
+                raise ValueError("categories must be of type List[str]!")
+                        
+            # Check values to be of type str
+            for x in categories:
+                if not isinstance(x, str):
+                    raise ValueError("categories must be of type List[str]!")
+            
+            query['categories'] = categories        
+        
+        # Validate if city_ids are valid
+        if kwargs.get('city_ids'):
+            city_ids: List[int] = kwargs.get('city_ids')
+            if not isinstance(city_ids, List):
+                raise ValueError(f"city_ids must be of type List[int]!")
+            
+            # Check values to be of type str
+            for x in city_ids:
+                if not isinstance(x, int):
+                    raise ValueError("city_ids must be of type List[int]!")
+                
+            query['city_ids'] = city_ids    
+            
+        # Validate if start_date is valid
+        if kwargs.get('start_date'):
+            start_date:datetime = kwargs.get('start_date')
+            if not isinstance(start_date, datetime):
+                raise ValueError(f"start_date must be of type datetime!")
+            
+            query['date_from'] = start_date.strftime('%Y-%m-%d')  
+            query['time_from'] = start_date.strftime('%H:%M')
+        
+        # Validate if end_date is valid
+        if kwargs.get('end_date'):
+            end_date:datetime = kwargs.get('end_date')
+            if not isinstance(end_date, datetime):
+                raise ValueError(f"end_date must be of type datetime!")
+            
+            query['date_to'] = end_date.strftime('%Y-%m-%d')  
+            query['time_to'] = end_date.strftime('%H:%M')
+        
+        # Validate if in_stock is valid
+        if kwargs.get('in_stock'):
+            in_stock:bool = kwargs.get('in_stock')
+            if not isinstance(in_stock, bool):
+                raise ValueError(f"in_stock must be of type bool!")
+            
+            query['in_stock'] = in_stock
+        
+        return query        
 
     def explore_attractions(self, search_term: str, page: int = 1) -> Dict:
-        """This function returns a requested page for the given search_term of the attractions endpoint.
-
-        Args:
-            search_term (str): Search term to be querried.
-            page (int, optional): Page number to fetch. Defaults to 1.
-
-        Returns:
-            Dict: Returns the requested page with meta data.
-        """
+        # AKA: Events / Artists etc... will give an artist
+        # TODO: Update function
         self._validate_search_term(search_term)
         
         r: requests.Response = self.session.get(
@@ -47,17 +115,9 @@ class EventimExploration:
         r.raise_for_status()
         return r.json()
 
-    def content(self, search_term: str, page: int = 1):
-        """This function returns a requested page for the given search_term of the content endpoint.
-
-        Args:
-            search_term (str): Search term to be querried.
-            page (int, optional): Page number to fetch. Defaults to 1.
-
-        Returns:
-            Dict: Returns the requested page with meta data.
-        """
-        self._validate_search_term(search_term)
+    def explore_content(self, search_term: str, page: int = 1):
+        # TODO: Update function
+        #self._validate_search_term(search_term)
         
         r: requests.Response = self.session.get(
             f"{self.endpoint}/v1/content",
@@ -66,17 +126,10 @@ class EventimExploration:
         r.raise_for_status()
         return r.json()
 
-    def locations(self, search_term: str, page: int = 1):
-        """This function returns a requested page for the given search_term of the locations endpoint.
-
-        Args:
-            search_term (str): Search term to be querried.
-            page (int, optional): Page number to fetch. Defaults to 1.
-
-        Returns:
-            Dict: Returns the requested page with meta data.
-        """
-        self._validate_search_term(search_term)
+    def explore_locations(self, search_term: str, page: int = 1):
+        # TODO: Update function
+        # self._validate_search_term(search_term)
+        
         r: requests.Response = self.session.get(
             f"{self.endpoint}/v1/locations",
             params={"search_term": search_term, "page": page},
@@ -84,21 +137,39 @@ class EventimExploration:
         r.raise_for_status()
         return r.json()
 
-    def product_groups(self, search_term: str, page: int = 1):
-        """This function returns a requested page for the given search_term of the product_groups endpoint.
+    def explore_product_groups(
+            self, 
+            search_term: str = None, 
+            categories: List['str'] = None,
+            city_ids: List[int] = None,
+            start_date:datetime = None,
+            end_date:datetime = None,
+            page: int = 1, 
+            sort: Literal['DateAsc','DateDesc' , 'NameAsc', 'NameDesc' , 'Rating' , 'Recommendation' ] = 'DateAsc',
+            in_stock:bool = True,
+        ):
+        #TODO: Documentation
+        params: Dict[str, Any] = self._build_query_parameters(
+            search_term=search_term,
+            categories=categories,
+            city_ids=city_ids,
+            start_date=start_date,
+            end_date=end_date,
+            page=page,
+            sort=sort,
+            in_stock=in_stock,
+        )
         
-        Args:
-            search_term (str): Search term to be querried.
-            page (int, optional): Page number to fetch. Defaults to 1.
-
-        Returns:
-            Dict: Returns the requested page with meta data.
-        """
-        self._validate_search_term(search_term)
+        print(params)
+        
         r: requests.Response = self.session.get(
             f"{self.endpoint}/v2/productGroups",
-            params={"search_term": search_term, "page": page},
+            params=params,
         )
         
         r.raise_for_status()
+        print(r.request.url)
         return r.json()
+    
+    
+        
--- tests/test_public_explorer.py
+++ tests/test_public_explorer.py
@@ -1,29 +1,107 @@
+import pytest
 from pyventim.public import EventimExploration 
 
-explorer: EventimExploration = EventimExploration()
-search_term:str = "Sleep"
-    
-def test_explore_content():
-    # We check for needed keys
-    needed_keys = ['content', 'results', 'totalResults', 'page', 'totalPages', '_links']
-    json = explorer.content(search_term)
-    assert(x in needed_keys for x in json.keys())
+import datetime
 
-def test_attractions():
-    # We check for needed keys
-    needed_keys = ['attractions', 'results', 'totalResults', 'page', 'totalPages', '_links']
-    json = explorer.attractions(search_term)
-    assert(x in needed_keys for x in json.keys())
+explorer: EventimExploration = EventimExploration()
+
+search_term = "Disneys DER KÖNIG DER LÖWEN"
+categories = ['Musical & Show|Musical']
+days = 7
+sort = "DateAsc"
+in_stock = False
+start_date = datetime.datetime.now()
+end_date = start_date + datetime.timedelta(days=days)
     
-def test_locations():
-    # We check for needed keys
-    needed_keys = ['locations', 'results', 'totalResults', 'page', 'totalPages', '_links']
-    json = explorer.locations(search_term)
-    assert(x in needed_keys for x in json.keys())
+# def test_explore_content():
+#     # We check for needed keys
+#     needed_keys = ['content', 'results', 'totalResults', 'page', 'totalPages', '_links']
+#     json = explorer.explore_content(search_term)
+#     assert(x in needed_keys for x in json.keys())
+
+# def test_explore_attractions():
+#     # We check for needed keys
+#     needed_keys = ['attractions', 'results', 'totalResults', 'page', 'totalPages', '_links']
+#     json = explorer.explore_attractions(search_term)
+#     assert(x in needed_keys for x in json.keys())
+    
+# def test_explore_locations():
+#     # We check for needed keys
+#     needed_keys = ['locations', 'results', 'totalResults', 'page', 'totalPages', '_links']
+#     json = explorer.explore_locations(search_term)
+#     assert(x in needed_keys for x in json.keys())
         
 
-def test_product_groups():
+def test_explore_product_groups():    
+    json = explorer.explore_product_groups(
+        search_term=search_term,
+        categories=categories,
+        start_date=start_date,
+        end_date=end_date,
+        sort=sort,
+        in_stock=in_stock
+    )
+    
     # We check for needed keys
     needed_keys = ['productGroups', 'results', 'totalResults', 'page', 'totalPages', '_links']
-    json = explorer.product_groups(search_term)
+    json = explorer.explore_product_groups(search_term)
     assert(x in needed_keys for x in json.keys())
+    
+
+# Query parameter validation tests
+def test_build_query_parameters_required_parameters():
+    with pytest.raises(ValueError, match = 'Must have search_term, categories or city_ids in the query parameters!'):
+        explorer._build_query_parameters()        
+
+def test_build_query_parameters_page_parameter():
+    with pytest.raises(ValueError, match = 'page must be a positive integer > 0!'):
+        explorer._build_query_parameters(search_term = search_term, page = -1)
+
+def test_build_query_parameters_sort_parameter():
+    with pytest.raises(ValueError, match = 'sort must be one of the following values: "DateAsc", "DateDesc", "NameAsc", "NameDesc", "Rating", "Recommendation"!'):
+        explorer._build_query_parameters(search_term = "ABC", sort = "This should fail.")
+        
+def test_build_query_parameters_search_term_parameter():
+    with pytest.raises(ValueError, match = 'search_term must have atleast two characters!'):
+        explorer._build_query_parameters(search_term = "A")
+    
+def test_build_query_parameters_categories_parameter():    
+    with pytest.raises(ValueError, match = 'categories must be of type List\[str\]!'):
+        # Variable should be a list.
+        explorer._build_query_parameters(categories = "abcs")
+        
+    with pytest.raises(ValueError, match = 'categories must be of type List\[str\]!'):
+        # All values in the list must be str   
+        explorer._build_query_parameters(categories = [123, "abcs"])     
+    
+    with pytest.raises(ValueError, match = 'categories must be of type List\[str\]!'):
+        # All values in the list must be str   
+        explorer._build_query_parameters(categories = [123, 234])  
+        
+def test_build_query_parameters_city_ids_parameter():    
+    with pytest.raises(ValueError, match = 'city_ids must be of type List\[int\]!'):
+        # Variable should be a list.
+        explorer._build_query_parameters(city_ids = 123)
+        
+    with pytest.raises(ValueError, match = 'city_ids must be of type List\[int\]!'):
+        # All values in the list must be str   
+        explorer._build_query_parameters(city_ids = [123, "abcs"])     
+    
+    with pytest.raises(ValueError, match = 'city_ids must be of type List\[int\]!'):
+        # All values in the list must be str   
+        explorer._build_query_parameters(city_ids = ["123", "234"])  
+        
+def test_build_query_parameters_start_date_parameter():
+    with pytest.raises(ValueError, match = 'start_date must be of type datetime!'):
+        # Variable should be a list.
+        explorer._build_query_parameters(search_term = search_term, start_date="This is not a valid timestamp")
+ 
+def test_build_query_parameters_end_date_parameter():
+    with pytest.raises(ValueError, match = 'end_date must be of type datetime!'):
+        # Variable should be a list.
+        explorer._build_query_parameters(search_term = search_term, end_date="This is not a valid timestamp")
+
+def test_build_query_parameters_in_stock_parameter():
+    with pytest.raises(ValueError, match = 'in_stock must be of type bool!'):
+        # Variable should be a list.
+        explorer._build_query_parameters(search_term = search_term, in_stock="This is not a valid boolean")
