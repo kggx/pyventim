fad82ff2b29d099f2c8ac41503adb33ac741ff8e
Kilian Braun <hello@kilianbraun.de>
2024-05-20 12:02:39 +0200

    added functions to handle html responses from component endpoint

--- src/pyventim/utils.py
+++ src/pyventim/utils.py
@@ -1,7 +1,8 @@
 import pathlib
+from typing import Dict, List, Any
 
-# import lxml
-# import json
+import lxml, lxml.html
+import json
 
 
 def parse_city_name_from_link(city_url: str) -> str:
@@ -30,17 +31,44 @@ def parse_city_id_from_link(city_url: str) -> int:
     return int(pathlib.Path(city_url).parts[3].split("-")[1])
 
 
-# def get_attraction_music_events_from_html(html: str):
-#     """This function returns all json entries on a html string
+def parse_json_from_component_html(html: str) -> List[Dict[str, Any]]:
+    """This function returns all json entries on a component html string
 
-#     Args:
-#         html (str): HTML to look for json data
+    Args:
+        html (str): HTML to look for json data
 
-#     Returns:
-#         Dict: Returns a list of dictionaries containing the json
-#     """
-#     html_obj: lxml.ETree = lxml.html.fromstring(html)
-#     return [
-#         json.loads(x.text)
-#         for x in html_obj.findall(".//script[@type='application/ld+json']")
-#     ]
+    Returns:
+        Dict: Returns a list of dictionaries containing the json
+    """
+    return [
+        json.loads(x.text)
+        for x in lxml.html.fromstring(html).findall(
+            ".//script[@type='application/ld+json']"
+        )
+    ]
+
+
+def parse_has_next_page_from_component_html(html: str) -> bool:
+    """This function returns if the returned page has a next page.
+
+    Args:
+        html (str): HTML code to check
+
+    Returns:
+        bool: Returns True if the page has a proceeding page.
+    """
+    return (
+        len(lxml.html.fromstring(html).findall(".//button[@data-qa='next-page']")) == 0
+    )
+
+
+# <button
+# class="btn btn-lg btn-square theme-interaction-border theme-interaction-bg standard-gray-border disabled"
+# title="N&auml;chste Seite"
+# data-qa="next-page"
+# disabled
+# >
+# <i class="icon icon-arrow-right">
+# <span class="sr-only">Nächste Seite</span>
+# </i>
+# </button>
