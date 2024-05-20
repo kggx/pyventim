be27e97b8b21d48acee0d93a1761ec4cd59188ea
Kilian Braun <hello@kilianbraun.de>
2024-05-20 11:21:04 +0200

    fixed utils module to be a function library

--- src/pyventim/utils.py
+++ src/pyventim/utils.py
@@ -4,32 +4,30 @@ import pathlib
 # import json
 
 
-class utils:
-    @classmethod
-    def parse_city_name_from_link(cls, city_url: str) -> str:
-        """This function returns the city name given a link like:
-        "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880"
+def parse_city_name_from_link(city_url: str) -> str:
+    """This function returns the city name given a link like:
+    "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880"
 
-        Args:
-            city_url (str): Link to parse
+    Args:
+        city_url (str): Link to parse
 
-        Returns:
-            str: The city name from the url.
-        """
-        return pathlib.Path(city_url).parts[3].split("-")[0]
+    Returns:
+        str: The city name from the url.
+    """
+    return pathlib.Path(city_url).parts[3].split("-")[0]
 
-    @classmethod
-    def parse_city_id_from_link(cls, city_url: str) -> int:
-        """This function returns the city id given a link like:
-        "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880"
 
-        Args:
-            city_url (str): Link to parse
+def parse_city_id_from_link(city_url: str) -> int:
+    """This function returns the city id given a link like:
+    "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880"
 
-        Returns:
-            int: The city id from the url.
-        """
-        return int(pathlib.Path(city_url).parts[3].split("-")[1])
+    Args:
+        city_url (str): Link to parse
+
+    Returns:
+        int: The city id from the url.
+    """
+    return int(pathlib.Path(city_url).parts[3].split("-")[1])
 
 
 # def get_attraction_music_events_from_html(html: str):
--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -2,16 +2,16 @@
 import pytest
 
 # Module to test
-from pyventim.utils import *
+import pyventim.utils
 
 
 def test_parse_city_name_from_link():
     url = "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880"
     expected_name = "hamburg"
-    returned_name = utils.parse_city_name_from_link(city_url=url)
+    returned_name = pyventim.utils.parse_city_name_from_link(city_url=url)
 
 
 def test_parse_city_id_from_link():
     url = "https://www.eventim.de/city/hamburg-7/venue/stage-theater-im-hafen-hamburg-3880"
     expected_name = 1
-    returned_name = utils.parse_city_id_from_link(city_url=url)
+    returned_name = pyventim.utils.parse_city_id_from_link(city_url=url)
